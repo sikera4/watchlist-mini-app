b7c557d91c443070ebdc09f4d61b947606bfd207
andrey.sikera <andrey.sikera@onetwotrip.com>
2025-12-26 15:44:49 +0300

    register haptic feedback on route change

--- src/components/ui/Navigation.tsx
+++ src/components/ui/Navigation.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useRegisterHapticFeedback } from '@/hooks/useRegisterHapticFeedback';
 import { Tab, Tabs } from '@heroui/react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -12,9 +13,15 @@ const navItems = [
 const Navigation = () => {
   const pathname = usePathname();
 
+  const registerHapticFeedback = useRegisterHapticFeedback();
+
   return (
     <nav className="pb-4 fixed bottom-0 left-0 right-0 flex justify-center pointer-events-none">
-      <Tabs selectedKey={pathname} className="pointer-events-auto">
+      <Tabs
+        selectedKey={pathname}
+        className="pointer-events-auto"
+        onSelectionChange={() => registerHapticFeedback('soft')}
+      >
         {navItems.map(({ route, label }) => (
           <Tab key={route} title={<Link href={route}>{label}</Link>} />
         ))}
