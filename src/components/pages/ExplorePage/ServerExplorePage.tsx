86f186e1f4049a7fcac4d69d5943788588294c20
andrey.sikera <andrey.sikera@onetwotrip.com>
2026-01-15 19:38:57 +0300

    use use cache for explore page

--- src/components/pages/ExplorePage/ServerExplorePage.tsx
+++ src/components/pages/ExplorePage/ServerExplorePage.tsx
@@ -6,8 +6,12 @@ import {
 import { getQueryClient } from '@/utilities/getQueryClient';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import ExplorePage from './ExplorePage';
+import { cacheLife } from 'next/cache';
 
 const ServerExplorePage = async () => {
+  'use cache';
+  cacheLife('hours');
+
   const queryClient = getQueryClient();
 
   const [moviesGenresList, tvShowsGenresList] = await Promise.all([
